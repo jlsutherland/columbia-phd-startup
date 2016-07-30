ba3411463e8f3c9eebffa2cb2542e29a7a993257
Joe Sutherland <jlsutherland@users.noreply.github.com>
2016-07-30 15:51:03 -0500

    Update wip.sh

--- wip.sh
+++ wip.sh
@@ -112,3 +112,34 @@ brew cask install ${fonts[@]}
 
 
 ```
+brew tap caskroom/cask
+brew install brew-cask
+ 
+brew install Caskroom/cask/xquartz
+brew cask install java
+ 
+brew tap homebrew/science
+brew install R
+ 
+brew install Caskroom/cask/rstudio
+ 
+ 
+# For latex:
+ 
+brew cask install mactex
+ 
+# OR YOU CAN DO
+ 
+brew cask install basictex # suggested by @noamross
+ 
+# DO NOT DO BOTH!
+ 
+brew install libsvg curl libxml2 gdal geos boost
+ 
+R CMD javareconf JAVA_CPPFLAGS=-I/System/Library/Frameworks/JavaVM.framework/Headers
+ 
+brew tap caskroom/fonts
+brew cask install font-fira-code
+brew cask install iterm2
+brew cask install gitup           # if you want a GUI for git stuff (h/t @jennybryan)
+```
