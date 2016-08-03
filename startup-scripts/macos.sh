3212cd88af5bf0e7c1da0f26cf1d14996c044a0e
Joe Sutherland <jls124@icloud.com>
2016-08-02 22:56:44 -0500

    adding brew cleanup

--- startup-scripts/macos.sh
+++ startup-scripts/macos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# To get homebrew, need xcode.
+# To get homebrew, need xcode cli tools.
 xcode-select --install
 
 # Check for Homebrew,
@@ -19,6 +19,7 @@ brew install git R libsvg libxml2 gdal geos boost pandoc
 # Install apps we want.
 brew cask install qlmarkdown java mactex
 brew cask install caskroom/cask/rstudio
+brew cleanup
 
 # Tell R where java is.
 R CMD javareconf JAVA_CPPFLAGS=-I/System/Library/Frameworks/JavaVM.framework/Headers
