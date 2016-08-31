7c6dcefcdbf36f4691655c3793c53386545a4b39
Joe Sutherland <jls124@icloud.com>
2016-08-31 13:25:47 -0400

    mac os prompts on screen

--- startup-scripts/macos.sh
+++ startup-scripts/macos.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
-# To get homebrew, need xcode cli tools.
-xcode-select --install
+echo ""
+echo "#################################################################################"
+echo "#################################################################################"
+echo "##"
+echo "## Columbia PhD Start-up Kit"
+echo "## v0.1.0"
+echo "## Built August 7, 2016"
+echo "##"
+echo "## Author: Joe Sutherland <joseph.sutherland@columbia.edu>"
+echo "##         *Please feel free to contact for support*"
+echo "##"
+echo "## NOTE: You may have to enter your password to run this script."
+echo "##"
+echo "#################################################################################"
+echo "#################################################################################"
+echo ""
+echo ""
+echo "Hello, PhD student! I will install the following on to your system:"
+echo ""
+echo "  * Homebrew -- a package manager for macOS"
+echo "  * Cakebrew -- a GUI for Homebrew"
+echo "  * TextMate2 -- an editor fluent in all languages"
+echo "  * R -- an open-source statistical computing platform"
+echo "  * git -- a versioning tool"
+echo "  * MacTeX -- a TeX distribution for mac, for writing beautiful papers"
+echo "  * TeXMaker -- a modern, cross-platform TeX editor"
+echo "  * R Studio -- a GUI for R, so you can easily pass your stat classes"
+echo "  * Pandoc -- a tool used by R Studio to convert between document formats"
+echo "  * Java 8 -- the latest Java distribution, required for some R packages"
+echo ""
+echo ""
 
 # Check for Homebrew,
 # Install if we don't have it.
